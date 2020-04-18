02cca714e3abd1b0cbd0e664d39b2b37932413e4
Glen Arrowsmith <glen@Glens-MacBook-Pro-2.local>
2020-04-18 20:16:39 +1000

    Begin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { loader } from 'graphql.macro';
+
+const query = loader('./foo.graphql');
+console.log('query', query);
 
 function App() {
   return (
