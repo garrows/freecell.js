b18ea6ac746c7fa30b743a1529df8b268865b604
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2023-03-13 23:13:23 +1000

    WIP v0.0.2

---
+++ build/precache-manifest.f9049063285158478cd9885bd7d1205b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45b139f16532c9acf69c02ecd9a22d02",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eb8ddb5ce977c8f3f137",
+    "url": "./static/css/2.8db15624.chunk.css"
+  },
+  {
+    "revision": "4966b930b8fd40e6b9e3",
+    "url": "./static/css/main.ac357f2d.chunk.css"
+  },
+  {
+    "revision": "eb8ddb5ce977c8f3f137",
+    "url": "./static/js/2.347a273e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.347a273e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4966b930b8fd40e6b9e3",
+    "url": "./static/js/main.61ac3c4a.chunk.js"
+  },
+  {
+    "revision": "af20a2617e0cdbd3aa22",
+    "url": "./static/js/runtime-main.c1a359ec.js"
+  },
+  {
+    "revision": "41ed0cbcfc78d8a922be11da1336633c",
+    "url": "./static/media/PerfectDOSVGA437Win.41ed0cbc.woff2"
+  },
+  {
+    "revision": "8577808ba810dc1e0bfc8f9c64056fbb",
+    "url": "./static/media/PerfectDOSVGA437Win.8577808b.woff"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "./static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "./static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "./static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "./static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
